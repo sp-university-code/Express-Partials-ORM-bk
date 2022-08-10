0b3693ca87dbf3bcd53e0ace7bddc00d7558127b
Jorge Zambrano <joraazam@espol.edu.ec>
2022-08-10 02:18:55 -0500

    Vista y tabla productos

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/productos', function(req, res, next) {
+  res.render('productos', { title: 'Productos' });
+});
+
 module.exports = router;
